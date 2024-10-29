e0bf95af7d4d7aae6829abda53fccefd5734cd99
Sofia5609 <komogorovasofia@mail.ru>
2024-10-29 10:47:29 +0700

    Добавлен поиск в массиве по аргументам, переданным программе

--- pr005.java
+++ pr005.java
@@ -309,6 +309,31 @@ class Arrays {
 		System.out.println("Список аргументов: ");
 		for (String s : args)
 			System.out.println(s);
+
+		System.out.println();
+		//Поиск строки в справочнике
+		String [][] numbers = {
+			{"Поликлиника", "269-55-55"},
+			{"Энергосбыт", "285-13-07"},
+			{"Управляющая компания", "200-57-93"},
+			{"Энергитическая компания", "313-13-13"}
+
+		};
+		int i12;
+
+		if(args.length != 1)
+			System.out.println("Задайте один аргумент при запуске программы");
+		else {
+			for(i12=0; i12<numbers.length; i12++) {
+				if(numbers[i12][0].equals(args[0])) {
+					System.out.println(numbers[i12][0] + ": " + numbers[i12][1]);
+				break;
+					}
+				}
+
+			if(i12 == numbers.length)
+				System.out.println("Строка не найдена");
+			}
 	}
 }	
 	
