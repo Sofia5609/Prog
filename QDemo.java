21545f1dc42c4c73974ba6fa55e9a1b3fea325c0
Sofia5609 <komogorovasofia@mail.ru>
2024-11-05 15:12:40 +0700

    Довавили конструкторы на базе массива и на базе другой очереди

--- QDemo.java
+++ QDemo.java
@@ -1,12 +1,34 @@
-//Очередьi
+//Очередь
 class Queue {
 	private char[] q;
 	private int putloc, getloc; //указатели на элементы очереди
 
+	//Конструктор, создающий очередь н6а базе массива с размером size
 	public Queue (int size) {
 		q = new char[size];
 		putloc = getloc = 0;
 
+	}
+	
+	//Конструктор очереди на основе объекта Queue
+	Queue(Queue ob) {
+		//установка показателей
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		//выделение места под массив
+		q = new char[ob.q.length];
+		//копирование элементов
+		for(int i=getloc; i < putloc ; i++)
+			q[i] = ob.q[i];
+	}
+	//Конструктор очереди с начальными значениями
+	Queue(char[] a) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+
+		for(int i=0; i<a.length; i++)
+			put(a[i]);
 	}
 
 	//Метод для помещения символа в очередь
@@ -49,6 +71,7 @@ class QDemo {
 		System.out.println("\n");
 		
 		//Использование очереди smallQ
+		System.out.println();
 		for(i=0; i< 5; i++) {
 			System.out.print("Попытка сохранения " + (char) ('Z' - i));
 			smallQ.put((char) ('Z' - i));
@@ -63,5 +86,39 @@ class QDemo {
 		
 		//bigQ.q[3] = 'E'; Попытка изменения значения у закрытой переменной класса Queue
 		//bigQ.putloc = 50; Попыткка изменения закрытого указателя в кшлассе Queue
+
+		Queue q1 = new Queue(10);
+		char[] name = {'I', 'v', 'a', 'n'};
+
+		Queue q2 = new Queue(name);
+
+//		char ch;
+		int i2;
+
+		for(i2=0; i2<10; i2++)
+			q1.put((char) ('A' + i2));
+		Queue q3 = new Queue(q1);
+
+		System.out.print("Содержимое q1: ");
+		for(i2 = 0; i2<10; i2++) {
+			ch = q1.get();
+			System.out.print(ch);
+		}
+		System.out.println("\n");
+
+		System.out.print("Содержимое q2(на базе массива): ");
+                for(i2 = 0; i2<4; i2++) {
+                        ch = q2.get();
+                        System.out.print(ch);
+                }
+                System.out.println("\n");
+		
+		System.out.print("Содержимое q3(на базе очереди q1): ");
+                for(i2 = 0; i2<10; i2++) {
+                        ch = q3.get();
+                        System.out.print(ch);
+                }
+                System.out.println("\n");
+		
 	}
 }
